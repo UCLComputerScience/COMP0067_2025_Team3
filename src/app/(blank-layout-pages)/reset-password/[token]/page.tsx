2fc0840424e4d79bbd1fa5ab9e2b30616dd14ba6
bielasekk <aleksandra.bielas.20@ucl.ac.uk>
2025-04-07 19:15:28 +0100

    npm build fix 3

--- src/app/(blank-layout-pages)/reset-password/[token]/page.tsx
+++ src/app/(blank-layout-pages)/reset-password/[token]/page.tsx
@@ -1,15 +1,13 @@
 import ResetPasswordPage from '@views/ResetPassword';
 
-interface PageProps {
-  params: {
-    token: string
-  }
-}
-
-const ResetPassword = async ({ params }: PageProps) => {
+const ResetPassword = async ({
+  params,
+}: {
+  params: { token: string };
+}) => {
   const { token } = params;
 
-  return <ResetPasswordPage token={token} />
-}
+  return <ResetPasswordPage token={token} />;
+};
 
 export default ResetPassword;
