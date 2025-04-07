6ebb42ce8569cf74e555419219f0c99bbbc864f8
bielasekk <aleksandra.bielas.20@ucl.ac.uk>
2025-04-07 20:30:56 +0100

    npm build fix please work this time

--- src/app/(blank-layout-pages)/forgotpassword/page.tsx
+++ src/app/(blank-layout-pages)/forgotpassword/page.tsx
@@ -1,14 +1,9 @@
 // Component Imports
 import ForgotPassword from '@views/ForgotPassword'
 
-// Server Action Imports
-import { getServerMode } from '@core/utils/serverHelpers'
-
 const ForgotPage = async () => {
-  // Vars
-  const mode = await getServerMode()
 
-  return <ForgotPassword mode={mode} />
+  return <ForgotPassword />
 }
 
 export default ForgotPage
