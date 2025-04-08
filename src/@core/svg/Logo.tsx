b4de01fdecf02ceb20272ed09b1b069b4e8af9a3
minjnlgc <kunyuquemian@outlook.com>
2025-04-08 19:56:15 +0100

    feat: header logo can change according to dark/light mode

--- src/@core/svg/Logo.tsx
+++ src/@core/svg/Logo.tsx
@@ -1,16 +1,22 @@
+'use client'
+
+import { useSettings } from '../hooks/useSettings'
 import Image from 'next/image'
 
-import SpiderLogo from '@components/layout/horizontal/spider_logo_1.png'
-
 const Logo = ({ className }: { className?: string }) => {
+  const { settings } = useSettings()
+
   return (
     <div className={className}>
-       <Image src={SpiderLogo} alt='Spider Logo' objectFit='cover' width={200} height={50} />
-
+      <Image
+        src={settings.mode === 'dark' ? '/images/logos/spider-logo-dark.png' : '/images/logos/spider-logo-light.png'}
+        alt='Spider Logo'
+        objectFit='cover'
+        width={200}
+        height={50}
+      />
     </div>
   )
 }
 
 export default Logo
-
-
