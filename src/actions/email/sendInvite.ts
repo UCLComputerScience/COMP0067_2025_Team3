f92a1460bfdb00e10ba2e572ad9da2f3681cf1af
bielasekk <aleksandra.bielas.20@ucl.ac.uk>
2025-04-08 15:50:10 +0100

    personalised invite email (#64)
    * personlised invite email
    
    * sorry, forgot to lint

--- src/actions/email/sendInvite.ts
+++ src/actions/email/sendInvite.ts
@@ -2,18 +2,42 @@
 
 import sgMail from '@sendgrid/mail';
 
+import { prisma } from '@/prisma/client'
+
+
+
+export interface User {
+  firstName: string
+  lastName: string
+}
+
+const getPatientName = async (id: string): Promise<User | null> => {
+  return await prisma.user.findFirst({
+    where: { id },
+    select: {
+      firstName: true,
+      lastName: true,
+    }
+  })
+}
+
+
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
-export async function sendInviteEmail(email: string) {
+export async function sendInviteEmail(name: string, email: string, patientId: string) {
+  const patient = await getPatientName(patientId);
+  const patientName = patient ? `${patient.firstName} ${patient.lastName}` : '';
+
   try {
     const msg = {
       to: email,
       from: process.env.SENDGRID_SENDER_EMAIL!,
       subject: 'You’ve been invited to join our platform!',
       html: `
-        <p>Dear clinician,</p>
-        <p>Your patient would like to share their symptom data with you on our platform. </p>
-        <p>Register your account to view their spider grams and track their data.</p>
+        <p>Dear ${name},</p>
+        <p>Your patient ${patientName} would like to share their symptoms data with you on our platform. </p>
+        <p>Register your account to view their spidergrams and track their data.</p>
         <p>Here is a link to our website: https://team3.uksouth.cloudapp.azure.com</p>
         <p>Kind regards,</p>
         <p>The Spider team</p>
